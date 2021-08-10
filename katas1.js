ac57b3bbb9c1ab2a5c6eaa8925a83cc068bf3584
anavaleska <avaleska2908@gmail.com>
2021-08-10 07:19:35 -0300

    Fix the Katas

--- katas1.js
+++ katas1.js
@@ -1,109 +1,111 @@
 function oneThroughTwenty() {
-
-   for (let i = 1; i <= 20; i++){
-    console.log(i);
+  let results = [];
+  for (let i = 1; i <= 20; i++) {
+    results.push(i);
   }
+  return results;
 }
-oneThroughTwenty();
- 
+console.log(oneThroughTwenty());
 
 function evensToTwenty() {
-    
-  for(let i = 2; i <= 20; i++){
-      if ((i % 2) == 0){
-        console.log(i);
-      }
-      
+  let results = [];
+  for (let a = 2; a <= 20; a++) {
+    if (a % 2 == 0) {
+      results.push(a);
     }
+  }
+  return results;
 }
-evensToTwenty();
-
+console.log(evensToTwenty());
 
 function oddsToTwenty() {
-  
-  for(let i = 1; i <= 20; i++){
-    if ((i % 2) == 1){
-      console.log(i);
+  let results = [];
+  for (let b = 1; b <= 20; b++) {
+    if (b % 2 == 1) {
+      results.push(b);
     }
-    
   }
+  return results;
 }
-oddsToTwenty();
-
+console.log(oddsToTwenty());
 
 function multiplesOfFive() {
-    
-  for(let i = 5; i <= 100; i++){
-    if ((i % 5) == 0){
-      console.log(i);
+  let results = [];
+  for (let c = 5; c <= 100; c++) {
+    if (c % 5 == 0) {
+      results.push(c);
     }
   }
-
+  return results;
 }
-multiplesOfFive();
-
+console.log(multiplesOfFive());
 
 function squareNumbers() {
-    
- for(let i = 1; i <= 100; i++){
-   if(Math.sqrt(i) % 1 === 0){
-    console.log(i);
-   }
- }
-    
-}
-squareNumbers();
+  let results = [];
 
+  for (let d = 1; d <= 100; d++) {
+    if (Math.sqrt(d) % 1 === 0) {
+      results.push(d);
+    }
+  }
+  return results;
+}
+console.log(squareNumbers());
 
 function countingBackwards() {
-    
-  for(let i = 20; i >= 1; i--){
-    console.log(i);
-  }
-}
-countingBackwards();
+  let results = [];
 
+  for (let e = 20; e >= 1; e--) {
+    results.push(e);
+  }
+  return results;
+}
+console.log(countingBackwards());
 
 function evenNumbersBackwards() {
- 
-  for(let i = 20; i >= 2; i--){
-    if((i % 2) == 0){
-      console.log(i);
+  let results = [];
+
+  for (let f = 20; f >= 2; f--) {
+    if (f % 2 == 0) {
+      results.push(f);
     }
   }
+  return results;
 }
-evenNumbersBackwards();
-
+console.log(evenNumbersBackwards());
 
 function oddNumbersBackwards() {
-    
-  for(let i = 20; i >= 1; i--){
-    if((i % 2) == 1){
-      console.log(i);
+  let results = [];
+
+  for (let g = 20; g >= 1; g--) {
+    if (g % 2 == 1) {
+      results.push(g);
     }
   }
+  return results;
 }
-oddNumbersBackwards();
-
+console.log(oddNumbersBackwards());
 
 function multiplesOfFiveBackwards() {
-    
- for(let i = 100; i >= 5; i--){
-   if((i % 5) == 0){
-     console.log(i);
-   }
- }
+  let results = [];
+
+  for (let h = 100; h >= 5; h--) {
+    if (h % 5 == 0) {
+      results.push(h);
+    }
+  }
+  return results;
 }
-multiplesOfFiveBackwards();
+console.log(multiplesOfFiveBackwards());
 
 function squareNumbersBackwards() {
-    
-   for(let i = 100; i >= 1; i--){
-     if (Math.sqrt(i) % 1 === 0){
-       console.log(i);
-     }
-   }
+  let results = [];
+
+  for (let j = 100; j >= 1; j--) {
+    if (Math.sqrt(j) % 1 === 0) {
+      results.push(j);
+    }
+  }
+  return results;
 }
-squareNumbersBackwards();
-
-
+console.log(squareNumbersBackwards());
